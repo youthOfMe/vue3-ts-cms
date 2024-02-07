9d2b25dc84c8b8551d2eab0189a7dc253b38cf55
youthOfMe <204069080@qq.com>
2024-02-07 18:52:37 +0800

    feat: 进行基本的样式重置 引入less”

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+import 'normalize.css'
+import './assets/css/index.less'
+
 import App from './App.vue'
 
 createApp(App).mount('#app')
