6f9df039262eaf196e67cabaef7d6751b5c99c80
youthOfMe <2040690180@qq.com>
2024-02-09 22:20:59 +0800

    feat: 进行双向绑定数据 准备登录数据

--- src/views/login/c-cpns/PaneAccount.vue
+++ src/views/login/c-cpns/PaneAccount.vue
@@ -1,14 +1,22 @@
 <template>
-  <el-form label-width="80px">
+  <el-form label-width="80px" status-icon :model="account">
     <el-form-item label="Username">
-      <el-input />
+      <el-input v-model="account.name" />
     </el-form-item>
     <el-form-item label="Password">
-      <el-input />
+      <el-input v-model="account.password" />
     </el-form-item>
   </el-form>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+// 1. 定义account数据
+const account = reactive({
+  name: '',
+  password: '',
+})
+</script>
 
 <style scoped></style>
