f442af6a43f008ceba6786a237ebf723a4cb0031
youthOfMe <2040690180@qq.com>
2024-02-09 22:40:10 +0800

    feat: 配置自动注册element-plus样式库

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+// 导入自动导入element样式的插件
+import { createStyleImportPlugin, ElementPlusResolve } from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +18,18 @@ export default defineConfig({
     }),
     Components({
       resolvers: [ElementPlusResolver()],
+    }),
+    createStyleImportPlugin({
+      resolves: [ElementPlusResolve()],
+      libs: [
+        {
+          libraryName: 'element-plus',
+          esModule: true,
+          resolveStyle: (name: string) => {
+            return `element-plus/theme-chalk/${name}.css`
+          },
+        },
+      ],
     }),
   ],
   resolve: {
